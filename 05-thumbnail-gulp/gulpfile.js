72dc1056fbee74f59dcd93c56593ea523c8947be
rkiel <cygnus@pobox.com>
2015-08-16 21:53:45 -0400

    vagrant: finished tooling

--- 05-thumbnail-gulp/gulpfile.js
+++ 05-thumbnail-gulp/gulpfile.js
@@ -4,25 +4,36 @@ var vinylSource = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var buffer = require('vinyl-buffer');
 
 gulp.task('default', function() {
-  var bundler = watchify(browserify({
+  var bundler = browserify({
     entries:      ['./src/App.jsx'],
     transform:    [reactify],
     extensions:   ['.jsx'],
     debug:        true,
     packageCache: {},
     fullPaths:    true
-  }))
+  })
 
   function build(file) {
     if (file) { gulpUtil.log('Recompiling ' + file); }
 
-    return bundler
+    var stream = bundler
       .bundle()
       .on('error', gulpUtil.log.bind(gulpUtil, 'Browserfiy Error'))
       .pipe(vinylSource('main.js'))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest('./public'));
+
+    stream = stream
+      .pipe(rename('main.min.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(gulp.dest('./public'));
+
+    return stream;
   }
 
   build();
